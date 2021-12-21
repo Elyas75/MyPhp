a9a1b92b13bf26ce9ec9ed9b26295bdf6d6a0007
Elyas75 <elyasharket@yahoo.fr>
2021-12-21 11:43:01 +0100

    C'est un bon Mardi

--- contact.php
+++ contact.php
@@ -6,7 +6,8 @@ require 'header.php'; ?>
 
 <h2>Nous contacter</h2>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, adipisci! Aspernatur quisquam illo aliquid unde. At ut fugiat molestiae deleniti, beatae iste itaque optio nobis! Blanditiis magni sapiente maiores dicta!
-Ipsa modi tenetur, ut eos, i excepturi porro aut qui natus totam ducimus, doloribus ipsam dolorem, sunt cupiditate laborum? Similique nobis iefx, oiex, oziejoxized oizjex  eaque consectetur repudiandae culpa. Distinctio officiis excepturi nulla cum?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores in, fuga, sit voluptatem odio, accusantium eius neque a accusamus ipsam rem quod amet. Quisquam voluptatem reprehenderit maxime aperiam explicabo eveniet?
+Sed ipsam, similique vitae ducimus fugiat ex sint non deleniti porro veniam quos natus maiores pariatur labore assumenda, sequi fuga nesciunt amet necessitatibus doloremque esse repellendus dicta saepe atque. Laudantium!
+Blanditiis omnis debitis reiciendis magni libero ea aspernatur quo, modi perspiciatis iusto doloremque pariatur? Id, atque ipsam facilis magni nihil quibusdam? Error voluptates ratione quia nam maxime quod et veniam?</p>
 
 <?php require 'footer.php'; ?>
