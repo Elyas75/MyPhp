a080e9ef667873b758fd5a39be1e00f6b28fe1b4
Elyas75 <elyasharket@yahoo.fr>
2021-12-21 11:36:25 +0100

    Hello

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php 
+
+$title = "Nous contacter";
+
+require 'header.php'; ?>
+
+<h2>Nous contacter</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, adipisci! Aspernatur quisquam illo aliquid unde. At ut fugiat molestiae deleniti, beatae iste itaque optio nobis! Blanditiis magni sapiente maiores dicta!
+Ipsa modi tenetur, ut eos, i excepturi porro aut qui natus totam ducimus, doloribus ipsam dolorem, sunt cupiditate laborum? Similique nobis iefx, oiex, oziejoxized oizjex  eaque consectetur repudiandae culpa. Distinctio officiis excepturi nulla cum?</p>
+
+<?php require 'footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$title = "Page d'accueil";
+
+require 'header.php'; 
+?>
+
+  <div class="bg-light p-5 rounded">
+    <h1>Navbar example</h1>
+    <p class="lead">This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page.</p>
+    <a class="btn btn-lg btn-primary" href="/docs/5.1/components/navbar/" role="button">View navbar docs &raquo;</a>
+  </div>
+</main>
+
+
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+      
+  </body>
+</html>
+    
